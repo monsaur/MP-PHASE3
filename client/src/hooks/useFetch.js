9ac41b7d376ccc60dc4f21dcc4ecf3ae7e28cb8c
monsaur <137795238+monsaur@users.noreply.github.com>
2023-08-06 14:20:52 +0800

    Add files via upload

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -10,7 +10,7 @@ const useFetch = (url) => {
     const fetchData = async () => {
       setLoading(true);
       try {
-        const res = await axios.get("https://ccapdev-api.onrender.com");
+        const res = await axios.get(url);
         setData(res.data);
       } catch (err) {
         setError(err);
@@ -23,7 +23,7 @@ const useFetch = (url) => {
   const reFetch = async () => {
     setLoading(true);
     try {
-      const res = await axios.get("https://ccapdev-api.onrender.com");
+      const res = await axios.get(url);
       setData(res.data);
     } catch (err) {
       setError(err);
